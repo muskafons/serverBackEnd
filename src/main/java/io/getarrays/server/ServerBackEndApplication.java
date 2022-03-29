74e498876d22d2e45af87449d64ebfa5421903f0
muskafons <muskafons@hotmail.com>
2022-03-28 21:00:04 -0500

    inicio de aplicativo backend

---
+++ src/main/java/io/getarrays/server/ServerBackEndApplication.java
@@ -0,0 +1,13 @@
+package io.getarrays.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBackEndApplication.class, args);
+	}
+
+}
