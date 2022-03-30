94740a64a779b836534b8266cbeabe6dc47a62d6
muskafons <muskafons@hotmail.com>
2022-03-30 18:27:09 -0500

    se agrega put actualizar (test good)

--- src/main/java/io/getarrays/server/controlador/ServerResource.java
+++ src/main/java/io/getarrays/server/controlador/ServerResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,12 @@ public class ServerResource {
 	public Server saveServer(@RequestBody @Valid Server server) {
 		return serverServicesImplementacion.create(server);
 	}
+	
+	@PutMapping("actualizar")
+	public Server updateServer(@RequestBody @Valid Server server) {
+		return serverServicesImplementacion.update(server);
+	}
+	
 	
 	@DeleteMapping("delete/{id}")
 	public Boolean deleteServerById(@PathVariable("id") Long id) {
